38a814e7e21f539fd4644a591ff0ed4ce6e846d2
giggs-lynx <giggs.tsai@gmail.com>
2021-12-31 15:05:57 +0800

    cargo fmt `gigasecond`

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,5 +1,5 @@
-use time::PrimitiveDateTime as DateTime;
 use std::time::Duration;
+use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
