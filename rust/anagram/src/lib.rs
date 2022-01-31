79f2d0d25a67b1ae6bb8a01b27846ac64ad61028
giggs-lynx <giggs.tsai@gmail.com>
2022-01-31 08:11:57 +0800

    update exercise `anagram` solution.

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,20 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let w_len = word.len();
+    let w_lower = word.to_lowercase();
+    let w_chars = transform(word);
+
+    possible_anagrams
+        .iter()
+        .map(|o| *o)
+        .filter(|o| w_len == o.len() && w_lower != o.to_lowercase() && w_chars == transform(o))
+        .collect()
+}
+
+fn transform(word: &str) -> Vec<char> {
+    let mut chars: Vec<char> = word.to_lowercase().chars().collect();
+    chars.sort_unstable();
+
+    chars
 }
